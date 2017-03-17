bab2d884af5b2cb85198c368f05e86582f736479
vagrant <alvin.cl.ng@gmail.com>
2017-03-17 00:08:22 +0000

    wtf

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
-
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
+app.use(cookieParser());
+
+
 
 function generateRandomString() {
   var text = "";
@@ -12,21 +15,19 @@ function generateRandomString() {
   text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
-
-}
+};
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const userName ={
+
+};
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -36,24 +37,47 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//find out whats the difference between app get and app post
-// how to push objects into ejs
-
-
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+   let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = {
+    shortURL: shortURL,
+    longURL: longURL,
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
   let newRandomString = generateRandomString();
   urlDatabase[newRandomString] = req.body['longURL']
-  res.redirect(`/urls/${newRandomString}`)
+  res.redirect(`/urls`)
   res.send("Ok");
 });
 
@@ -63,25 +87,25 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  // Need to edit these 2 lines
   const shortURL = req.params.shortURL
-  // short URL is now the key to my data base
-  // Look over this code
   urlDatabase[shortURL] = req.body.longURL
-  res.redirect(`/urls`)
+  res.redirect('/urls')
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect('/')
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/');
 })
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
-  //remind yourself what do these lines do
-  let templateVars = { shortURL: shortURL, longURL: longURL };
-  res.render("urls_show", templateVars);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
